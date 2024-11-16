cf3789b70f052e832d2ef336360ef7b188dfd129
Eric Vaughn <vaughner@oregonstate.edu>
2024-11-16 15:20:40 -0800

    sql

---
+++ db.sql
@@ -0,0 +1,23 @@
+SET FOREIGN_KEY_CHECKS = 0;
+SET AUTOCOMMIT = 0;
+
+CREATE OR REPLACE TABLE timingserviceUsers (
+  userID int AUTO_INCREMENT PRIMARY KEY,
+  secret varchar(255) UNIQUE NOT NULL,
+  address varchar(255),
+  timezone int
+);
+
+CREATE OR REPLACE TABLE timingserviceTimers (
+  timerID INT AUTO_INCREMENT PRIMARY KEY,
+  userID INT NOT NULL,
+  timerName varchar(255) NOT NULL,
+  time datetime NOT NULL,
+  payload JSON NOT NULL CHECK (json_valid(`payload`)),
+  UNIQUE (userID, timerName),
+  FOREIGN KEY (userID) REFERENCES timingserviceUsers (userID) 
+  ON DELETE CASCADE ON UPDATE CASCADE
+);
+
+SET FOREIGN_KEY_CHECKS = 1;
+SET AUTOCOMMIT = 1;
