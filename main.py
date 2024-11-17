1180c0757a6c7dabe1b9c452b7217f41a758abf0
Eric Vaughn <vaughner@oregonstate.edu>
2024-11-16 17:38:51 -0800

    added shell functions to main

--- main.py
+++ main.py
@@ -5,7 +5,12 @@ import subprocess
 import sys
 import zmq
 
+repSocket = None
+alarmProc = None
+
 def app(name : str, port : str) -> None:
+  global repSocket, alarmProc
+  
   try:
     repContext = zmq.Context()
     repSocket  = repContext.socket(zmq.REP)
@@ -28,7 +33,22 @@ def app(name : str, port : str) -> None:
 
   print(name + " running ...")
   while True:
-    pass
+    inMsg = None
+    try:
+      inMsg = repSocket.recv_json()
+
+    except Exception as e:
+      print("Exception raised receiving: ", e)
+      repSocket.send_json({
+          "type":"error", 
+          "payload":{
+              "status":"FAILED", 
+              "msg":"Json formatting error" + str(e)
+          }
+      })
+      continue
+
+    repSocket.send_json(wrapper(inMsg))
 
 def wrapper(request : object) -> object:
   requestType = request.get("type")
@@ -36,12 +56,64 @@ def wrapper(request : object) -> object:
     return request
   
   elif requestType == "set address":
-    return {}
+    return setAddress(request)
   
   elif requestType == "set timezone":
-    return {}
+    return setTimezone(request)
   
-  #elif requestType == 
+  elif requestType == "set timer":
+    return setTimer(request)
+
+  elif requestType == "set alarm":
+    return setAlarm(request)
+
+  elif requestType == "cancel":
+    return cancel(request)
+
+  elif requestType == "del":
+    return delete(request)
+
+  elif requestType == "get":
+    return get(request)
+
+  elif requestType == "get ids":
+    return getIds(request)
+
+  elif requestType == "get active":
+    return getActive(request)
+
+  elif requestType == "get history":
+    return getHistory(request)
+
+def setAddress(req : object) -> object:
+  return None
+
+def setTimezone(req : object) -> object:
+  return None
+
+def setTimer(req : object) -> object:
+  return None
+
+def setAlarm(req : object) -> object:
+  return None
+
+def cancel(req : object) -> object:
+  return None
+
+def delete(req: object) -> object:
+  return None
+
+def get(req: object) -> object:
+  return None
+
+def getIds(req : object) -> object:
+  return None
+
+def getActive(req : object) -> object:
+  return None
+
+def getHistory(req : object) -> object:
+  return None
 
 if __name__ == "__main__":
   args = sys.argv
